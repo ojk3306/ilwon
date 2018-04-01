1260d7d49f2f4225a572353f9a38e9f422a4244f
Choijiseok1 <gkffhqnd@naver.com>
2018-04-01 13:44:45 +0900

    ds

--- src/users/controller/InsertUserInfo.java
+++ src/users/controller/InsertUserInfo.java
@@ -27,7 +27,11 @@ public class InsertUserInfo extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		System.out.println(request.getParameter("user_type"));
+	System.out.println(request.getParameter("user_type"));//101이면 학생 102면 선생
+	
+	
+	
+	
 	}
 
 	/**
