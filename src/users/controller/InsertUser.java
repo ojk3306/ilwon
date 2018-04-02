e109af099280a180ee231b0be0bca2295c2f60eb
user1 <user1@KH_H>
2018-04-02 15:10:55 +0900

    123

--- src/users/controller/InsertUser.java
+++ src/users/controller/InsertUser.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import users.model.service.UsersService;
-import users.model.vo.User;
+
 import users.model.vo.Users;
 
 /**
